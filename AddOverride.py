ce197fa585228f2e1b56804f9ac99385260e3068
Creed Haymond <creedasaurus@gmail.com>
2017-02-07 15:35:43 -0700

    Ok. I am trying to use a regular expression to replace the end of a virtual function line with override. I think it's working, but I'm gonna have Tanner check it out.

--- AddOverride.py
+++ AddOverride.py
@@ -7,67 +7,18 @@ import argparse
 import os
 
 
-def fixFiles(arg):
-    fileLoc = []  # store the file locations
-    lineNum = []  # store the line numbers to edit
-    line = ""
-    index = -1
-    LinesChanged = 0
+def fixFiles(file_name):
+    find_line = re.compile("virtual [\w]+ [\w]+\(\);")
+    data = []
+    with open(file_name, 'r+') as in_file:
+        data = in_file.readlines()
 
-    # read in the list and parse to the appropriate arrays
-    with open() as changeList:
-        while True:
-            line = changeList.readline()
-            if not line:
-                break
-            tempList = re.split(',', line.strip('\n'))
-            fileLoc.append(tempList[0].strip())
-            lineNum.append(int(tempList[1]))
+    for indx, line in enumerate(data):
+        if find_line.search(line):
+            data[indx] = line.replace(';', ' override;')
 
-    # open the files
-    for elem in fileLoc:
-        index += 1
-        changeLn = lineNum[index]
-        lnNum = 1
-
-        if os.path.isfile(elem):
-            filedata = []
-
-            # Open File and Read Into an Array
-            with open(elem, 'r') as filet:
-                for line in filet:
-                    filedata.append(line)
-
-                    # Looping Through the ARRAY (Not File) by Index, and Making Changes as needed
-            for l_index in range(len(filedata)):
-                line = filedata[l_index]
-                stripped_line = line.strip()
-                if lnNum == changeLn:
-                    if "{" in stripped_line:
-                        filedata[l_index] = line.replace("{", "override {")
-                        LinesChanged += 1
-                    elif stripped_line.endswith(";") or "//" in line:
-                        tempstring = line.replace(";", " override;")
-                        filedata[l_index] = line.replace(";", " override;")
-                        LinesChanged += 1
-                    elif stripped_line.endswith(")"):  # foo() replace ")" with ") override"
-                        filedata[l_index] = line.replace(")", ") override")
-                        LinesChanged += 1
-                    elif stripped_line.endswith(
-                            "const"):  # replace "const" with "const override" this will catch const \n {
-                        filedata[l_index] = line.replace("const", "const override")
-                        LinesChanged += 1
-                lnNum += 1
-
-            # Opening File and Writing the Lines Back Out FROM the array
-            with open(elem, 'w') as filet:
-                for line in filedata:
-                    filet.write(line)
-        else:
-            print("File Doesn't Exist:" + elem)
-
-    print("--------------------------------")
-    print("    Ending program...    " + "Lines Changed: " + str(LinesChanged))
+    with open(file_name, 'w') as out_file:
+        out_file.writelines(data)
 
 
 def validate_file(pars, arg):
@@ -87,7 +38,7 @@ def validate_searchterm(pars, arg):
 
 
 if __name__ == "__main__":
-    print("in Main")
+    fixFiles("testing.txt")
     # parser = argparse.ArgumentParser(description="add_override v0.2")
     # parser.add_argument("-i", "--infile", metavar="FILE-DIR",
     #                     help="file or directory of target files to change",
